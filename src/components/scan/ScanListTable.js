394dea4e7b599a5a1b11c1d43db74ec36f2aea37
Pawel <pawel.bednarz2@gmail.com>
2024-04-06 12:51:15 +0200

    Implement whois lookup functionality

--- src/components/scan/ScanListTable.js
+++ src/components/scan/ScanListTable.js
@@ -1,5 +1,5 @@
 import {Table, Tooltip} from "@mantine/core";
-import {IconEye, IconFileSettings, IconPlayerPlay, IconTrash} from "@tabler/icons-react";
+import {IconEye, IconFileSettings, IconPlayerPlay, IconTrash, IconFileInfo } from "@tabler/icons-react";
 import {modals} from "@mantine/modals";
 import axios from "axios";
 import {API_URL} from "../../consts";
@@ -31,6 +31,32 @@ export const ScanListTable = (props) => {
                 </p>
             ),
         });
+    }
+
+    const showWhoisInformation = (scan) => {
+        // make request
+        axios.get(
+            API_URL + "/statistics/getWhoisInfo/" + scan.id,
+            {
+                headers: {
+                    "Authorization": "Bearer " + sessionStorage.getItem("token")
+                }
+            }
+        ).then(res => {
+            let obj = JSON.parse(res.data.whois)
+            let test = obj.name_servers.replaceAll("\r", "").replaceAll("\n", "").split(" ")
+            test = test.filter(n => n !== "")
+            obj.name_servers = test
+            modals.open({
+                title: 'Whois information of ' + scan.scannedDomain,
+                centered: true,
+                children: (
+                    <p style={{whiteSpace: "pre"}}>
+                        {JSON.stringify(obj, null, 4)}
+                    </p>
+                ),
+            });
+        }).catch(err => errorToast("Something went wrong."))
     }
 
     const deleteScan = (scan) => {
@@ -84,6 +110,12 @@ export const ScanListTable = (props) => {
                             <IconFileSettings
                                 className="scan-icon"
                                 onClick={() => showScanConfiguration(val)}
+                            />
+                        </Tooltip>
+                        <Tooltip label="Show whois information" color="gray">
+                            <IconFileInfo
+                                className="scan-icon"
+                                onClick={() => showWhoisInformation(val)}
                             />
                         </Tooltip>
                         <Tooltip label="Delete scan" color="gray">
